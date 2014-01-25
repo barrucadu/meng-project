cfc0898fd0183bcb4af164c69332655b30182249
Michael Walker <mike@barrucadu.co.uk>
2014-01-25 03:58:45 +0000

    Implement linked list functions for demo

---
+++ code/lists.c
@@ -0,0 +1,97 @@
+#include <stddef.h>
+
+#include "shared.h"
+#include "lists.h"
+
+#if GC == 0
+#include "armstrong-virding.h"
+#elif GC == 1
+#include "fenichel-yochelson.h"
+#else
+#error "GC must be 0 (for armstrong-virding) or 1 (for fenichel-yochelson)"
+#endif
+
+list* singleton(unsigned int data)
+{
+#if GC == 0
+  cell* out = alloc_atom_ptr(data, NULL);
+#else
+  cell* out = alloc();
+  out->car.tag = ATOM;
+  out->car.val.data = data;
+  out->cdr.tag = REFERENCE;
+  out->cdr.val.ptr = NULL;
+#endif
+
+  return out;
+}
+
+list* reverse(list* xs)
+{
+  cell* out = NULL;
+  for(cell* cur = xs; cur != NULL; cur = cur->cdr.val.ptr)
+    {
+      // To prevent out being thrown away by the garbage collector, we
+      // need to set it as a root before we allocate, and then remove
+      // once we're done.
+      if(out != NULL)
+        add_root(out);
+      component cdr = { .tag = REFERENCE, .val.ptr = out };
+#if GC == 0
+      cell* out2 = alloc(cur->car, cdr);
+#else
+      cell* out2 = alloc();
+      out2->car = xs->car;
+      out2->cdr = cdr;
+#endif
+      if(out != NULL)
+        remove_root(out);
+      out = out2;
+    }
+  return out;
+}
+
+list* append(list* xs, list* ys)
+{
+  cell* rxs = reverse(xs);
+  add_root(rxs);
+  cell* out = ys;
+
+  for(cell* cur = rxs; cur != NULL; cur = cur->cdr.val.ptr)
+    {
+      if(out != ys)
+        add_root(out);
+      component cdr = { .tag = REFERENCE, .val.ptr = out };
+#if GC == 0
+      cell* out2 = alloc(cur->car, cdr);
+#else
+      cell* out2 = alloc();
+      out2->car = xs->car;
+      out2->cdr = cdr;
+#endif
+      if(out != ys)
+        remove_root(out);
+      out = out2;
+    }
+    
+  return out;
+}
+
+list* head(list* xs)
+{
+#if GC == 0
+  cell* out = (xs->car.tag == ATOM) ? alloc_atom_ptr(xs->car.val.data, NULL) : alloc_ptr_ptr(xs->car.val.ptr, NULL);
+#else
+  cell* out = alloc();
+  out->car = xs->car;
+  out->cdr.tag = REFERENCE;
+  out->cdr.val.ptr = NULL;
+#endif
+
+  return out;
+}
+
+list* tail(list* xs)
+{
+  return xs->cdr.val.ptr;
+}
---
+++ code/lists.h
@@ -0,0 +1,34 @@
+#ifndef _LISTS_H
+#define _LISTS_H
+
+#include "shared.h"
+
+typedef cell list;
+
+/**
+ * Allocate a singleton list
+ */
+list* singleton(unsigned int data);
+
+/**
+ * Reverse a list
+ */
+list* reverse(list* xs);
+
+/**
+ * Create a new list by appending ys to xs. This allocates a totally
+ * new xs, to avoid mutating cdrs.
+ */
+list* append(list* xs, list* ys);
+
+/**
+ * Get the head of a list, as a singleton list
+ */
+list* head(list* xs);
+
+/**
+ * Get the tail of a list
+ */
+list* tail(list* xs);
+
+#endif
